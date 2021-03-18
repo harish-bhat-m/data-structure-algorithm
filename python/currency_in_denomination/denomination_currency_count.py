368b93e77a7f6f89b7e73265276954445c44a6ee
Harish Bhat <harish.bhat.m@gmail.com>
2021-03-18 21:36:53 +0530

    Resolved the pylint issues

--- python/currency_in_denomination/denomination_currency_count.py
+++ python/currency_in_denomination/denomination_currency_count.py
@@ -1,37 +1,52 @@
-
-class CurrencyCombination(object):
+""" main implementation of the logic """
+class CurrencyCombination():
     """ Implmentation to calculate the different combination of currency
         denomination for given amount of money"""
-    def __init__(self, amount, currency_combination):
-        """Initilization function"""
-        self.currency_combination = currency_combination
-        self.amount = amount
-        self.__calculate()
+    def __init__(self, rupee, denomination):
+        self.currency_combination = denomination
+        self.amount = rupee
+        self.result_list = []
 
-    def __calculate(self):
+    def calculate(self):
         """ Main functionality"""
-        print('Denomination are:{}'.format(self.currency_combination))
-        print("Entered amont of money is:{}".format(self.amount))
-        print("Currency combinations are as follows")
-        for i in range(len(self.currency_combination)):
+        no_of_iteration = len(self.currency_combination)
+        for i in range(no_of_iteration):
             if self.currency_combination[i] <= self.amount:
-                print("------------------------------------")
+                self.result_list.append("------------------------------------")
                 total = 0
                 j = i
                 reminder = self.currency_combination[i]
-                while j < len(self.currency_combination):
+                no_of_iteration = len(self.currency_combination)
+                while j < no_of_iteration:
                     if reminder >= self.currency_combination[j]:
                         remaining = self.amount - total
                         reminder = remaining % self.currency_combination[j]
                         currency_count = (remaining - reminder) / self.currency_combination[j]
                         total += currency_count * self.currency_combination[j]
-                        print("Denomination of {} in  {} number".\
+                        self.result_list.append("Denomination of {} in  {} number".\
                             format(self.currency_combination[j],currency_count))
                     j += 1
 
-demomination = [2000,500,100,50,20,10,5,2,1]
-try:
-    rupee = int(input("Enter the ruppes: "))
-    CurrencyCombination(rupee, demomination)
-except  Exception as error: 
-    print("Entered number is not integer. Please try again")
+    def display(self):
+        """ Display the results """
+        print('Denomination are:{}'.format(self.currency_combination))
+        print("Entered amont of money is:{}".format(self.amount))
+        print("Currency combinations are as follows")
+        for result in self.result_list:
+            print(result)
+
+
+def main():
+    """ main functionality to call the method"""
+    denomination = [2000,500,100,50,20,10,5,2,1]
+    try:
+        rupee = int(input("Enter the ruppes: "))
+        curr_combi_obj = CurrencyCombination(rupee, denomination)
+        curr_combi_obj.calculate()
+        curr_combi_obj.display()
+    except  ValueError  as error:
+        print(error)
+        print("Entered number is not integer. Please try again")
+
+if __name__ == "__main__":
+    main()
