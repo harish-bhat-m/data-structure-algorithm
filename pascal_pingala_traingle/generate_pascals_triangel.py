69490362b13fae82312e8be495b1c1ac449cb913
Harish Bhat <harish.bhat.m@gmail.com>
2021-04-02 15:45:49 +0530

    added the pascal triange generation algorithm

---
+++ pascal_pingala_traingle/generate_pascals_triangel.py
@@ -0,0 +1,24 @@
+""" Generation of pascal triange """
+class Pascal():
+    """ Implmentation of Pascal triangle"""
+    def __init__(self):
+        self.row = [1]
+        self.static_list = [0]
+        self.level = 0
+
+    def get_level(self):
+        """ Input from the user """
+        self.level = int(input("Enter the trianle level:"))
+
+    def generate_pascal_traingle(self):
+        """ traingle generation logic"""
+        for _ in range(self.level):
+            row_string = " ".join(str(element) for element in self.row)
+            print("{:^{width}}".format(row_string, width=self.level*6))
+            self.row = [left+right for left,right in \
+                        zip(self.row+self.static_list, self.static_list+self.row)]
+
+if __name__ == "__main__":
+    pascal_obj =Pascal()
+    pascal_obj.get_level()
+    pascal_obj.generate_pascal_traingle()
