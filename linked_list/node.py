b2e94b49966fc242567dd94a6de302f748c4834a
harish-bhat-m <harish.bhat.m@gmail.com>
2025-04-13 19:38:14 +0530

    Completed the node implementation

--- linked_list/node.py
+++ linked_list/node.py
@@ -26,3 +26,50 @@ class Node(object):
             self.next_node = None
         except AssertionError as error:
             print("Error Case: Element cannot be empty.")
+
+
+    def set_data(self, element=None):
+        """
+        Sets the value of the node.
+        :param: 
+            element: value of the node.
+        :returns: None
+        :rtype: None
+        :exception: If the element is none, the exception is raised.        
+        """
+        try:
+            assert (element is not None)
+            self.element = element
+        except AssertionError as error:
+            print("Error Case: Element cannot be empty.")
+
+    def get_data(self):
+        """
+        Returns the value of the node
+        :param: None
+        :retunr : None
+        :rtype: None
+        :exception: None.
+        """
+        return self.element
+    
+    def set_next(self, next_node):
+        """
+        Set the pointers to the next node. Links current node with next node.
+        :param: 
+            next_node: pointer to the next node.
+        :returns: None
+        :rtype: None
+        :exception: None.
+        """
+        self.next_node = next_node
+
+    def get_next(self):
+        """
+        Returns the pointers to the next node.
+        :param: None.
+        "returns: None.
+        :rtype: None.
+        :exception: None.
+        """
+        return self.next_node   
